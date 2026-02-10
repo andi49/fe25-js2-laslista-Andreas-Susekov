e49b522b6030a181061ea04b7afeb8d3f321235e
andi49 <46962871+andi49@users.noreply.github.com>
2026-02-10 11:57:17 +0100

    Update filterSort.js

--- module/filterSort.js
+++ module/filterSort.js
@@ -1,24 +1,27 @@
+
 export function sortBooks(books, option) {
+ let copy = [...books]
+ 
   books.sort((a, b) => {
     if (option === 'aa') return a.title.localeCompare(b.title, 'sv');
     if (option === 'ab') return b.title.localeCompare(a.title, 'sv');
   });
 }
 
-export function sortAuthor(books, option){
-    books.sort((a,b)=> {
+export function sortAuthor(copy, option){
+    copy.sort((a,b)=> {
         if (option === 'bb') return a.author.localeCompare(b.author, 'sv');
         else if(option === 'bc') return b.author.localeCompare(a.author, 'sv');
     })
 }
 
-export function newToldest(books, option) {
-    books.sort((a,b) => {
+export function newToldest(copy, option) {
+    copy.sort((a,b) => {
         if (option === 'cc') return b.key.localeCompare(a.key);
         else if(option === 'cd') return a.key.localeCompare(b.key);
     })
 }
 
-export function sortFavoriteFilter(books){
-return books.filter(boken => boken.favorite)
+export function sortFavoriteFilter(copy){
+return copy.filter(boken => boken.favorite)
 }
